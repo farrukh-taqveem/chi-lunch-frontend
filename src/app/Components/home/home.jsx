755ba3243d3d73039a6a25356ef47f31d37489e3
farrukh_taqveem <farrukh@cognitivehealthintl.com>
2020-10-30 22:25:13 +0500

    package json update

--- src/app/Components/home/home.jsx
+++ src/app/Components/home/home.jsx
@@ -1,43 +1,59 @@
 import React, { Component } from "react";
 import { Button } from "primereact/button";
-import { Card } from "primereact/card";
 import { ListBox } from "primereact/listbox";
-import Api from '../shared/apiService';
+import { InputNumber } from "primereact/inputnumber";
+import Api from "../shared/apiService";
+import './home.scss'
 
 class HomeComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            selectedUsers: null,
-            userList: []
-        }
-    }
-componentDidMount(){
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedUsers: null,
+      userList: [],
+      cost: 0,
+    };
+  }
+  componentDidMount() {
     this.getUsers();
-}
-getUsers = async()=>{
-    const resp = await Api.get('user');
-    if(resp.status === 200){
-        for (const user of resp.data.data){
-            user.fullName = user.firstName + ' ' + user.lastName;
-        }
-        const regularUsers = resp.data.data.filter(user=> user.type === 'Regular')
-        this.setState({userList: resp.data.data, selectedUsers: regularUsers})
+  }
+  getUsers = async () => {
+    const resp = await Api.get("user");
+    if (resp.status === 200) {
+      for (const user of resp.data.data) {
+        user.fullName = user.firstName + " " + user.lastName;
+      }
+      const regularUsers = resp.data.data.filter(
+        (user) => user.type === "Regular"
+      );
+      this.setState({ userList: resp.data.data, selectedUsers: regularUsers });
     }
-}
+  };
   render() {
     return (
-      <div>
-        <h4>Select Participants</h4>
-        <ListBox
-          value={this.state.selectedUsers}
-          options={this.state.userList}
-          onChange={(e) => this.setState({ selectedUsers: e.value })}
-          multiple
-          filter
-          optionLabel='fullName'
-          listStyle={{ maxHeight: "15vw" }}
-        />
+      <div className='p-grid home-page'>
+        <div className="p-col user-selector">
+          <h4>Select Participants</h4>
+          <ListBox
+            value={this.state.selectedUsers}
+            options={this.state.userList}
+            onChange={(e) => this.setState({ selectedUsers: e.value })}
+            multiple
+            filter
+            optionLabel='fullName'
+            listStyle={{ maxHeight: "15vw" }}
+          />
+        </div>
+        <div className='p-col'>
+          <div className='p-field'>
+            <label htmlFor='integeronly'>Cost: </label>
+            <InputNumber
+              id='integeronly'
+              value={this.state.cost}
+              onValueChange={(e) => this.setState({ cost: e.value })}
+            />
+          </div>
+        </div>
       </div>
     );
   }
