c6515fc38a574f8a7fdeeba17cebb505d600d9fc
farrukh_taqveem <farrukh@cognitivehealthintl.com>
2020-10-30 20:17:42 +0500

    package json

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/app';
 
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
